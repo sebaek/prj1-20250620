276b652c816f0303125a077ac2aad6f28932b4da
sebaek <sebaek@gmail.com>
2025-06-20 10:45:13 +0900

    글 목록

--- src/main/java/com/example/prj1/controller/BoardController.java
+++ src/main/java/com/example/prj1/controller/BoardController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("board")
@@ -26,7 +28,17 @@ public class BoardController {
     public String writePost(BoardForm data) {
 
         boardService.add(data);
-        
+
         return "board/write";
+    }
+
+    @GetMapping("list")
+    public String list(Model model) {
+
+        var result = boardService.list();
+
+        model.addAttribute("boardList", result);
+
+        return "board/list";
     }
 }
