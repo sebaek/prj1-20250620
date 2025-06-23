c458c8d802b2dfce7a8b3dd91f35262e1f3977e2
sebaek <sebaek@gmail.com>
2025-06-23 12:23:01 +0900

    회원 가입시 아이디가 이미 있으면 다시 가입 유도

--- src/main/java/com/example/prj1/member/controller/MemberController.java
+++ src/main/java/com/example/prj1/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.prj1.member.controller;
 import com.example.prj1.member.dto.MemberForm;
 import com.example.prj1.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,13 +27,22 @@ public class MemberController {
 
     @PostMapping("signup")
     public String signup(MemberForm data, RedirectAttributes rttr) {
-        // service
-        memberService.add(data);
+        try {
+            // service
+            memberService.add(data);
 
 
-        rttr.addFlashAttribute("alert",
-                Map.of("code", "success", "message", "회원 가입되었습니다."));
+            rttr.addFlashAttribute("alert",
+                    Map.of("code", "success", "message", "회원 가입되었습니다."));
 
-        return "redirect:/board/list";
+            return "redirect:/board/list";
+        } catch (DuplicateKeyException e) {
+            rttr.addFlashAttribute("alert",
+                    Map.of("code", "warning", "message", e.getMessage()));
+
+            rttr.addFlashAttribute("member", data);
+
+            return "redirect:/member/signup";
+        }
     }
 }
--- src/main/java/com/example/prj1/member/service/MemberService.java
+++ src/main/java/com/example/prj1/member/service/MemberService.java
@@ -4,8 +4,11 @@ import com.example.prj1.member.dto.MemberForm;
 import com.example.prj1.member.entity.Member;
 import com.example.prj1.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +17,23 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void add(MemberForm data) {
-        // 새 엔티티객체 생성해서
-        Member member = new Member();
-        // data에 있는 것 entity 에 옮겨 담고
-        member.setId(data.getId());
-        member.setPassword(data.getPassword());
-        member.setNickName(data.getNickName());
-        member.setInfo(data.getInfo());
 
-        // repository.save()
-        memberRepository.save(member);
+        Optional<Member> db = memberRepository.findById(data.getId());
+
+        if (db.isEmpty()) {
+            // 새 엔티티객체 생성해서
+            Member member = new Member();
+            // data에 있는 것 entity 에 옮겨 담고
+            member.setId(data.getId());
+            member.setPassword(data.getPassword());
+            member.setNickName(data.getNickName());
+            member.setInfo(data.getInfo());
+
+            // repository.save()
+            memberRepository.save(member);
+        } else {
+            throw new DuplicateKeyException(data.getId() + "는 이미 있는 아이디입니다.");
+        }
+
     }
 }
