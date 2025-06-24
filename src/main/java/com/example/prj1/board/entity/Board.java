1232572c137191d8503373c528ac7ebd171ffcaa
sebaek <sebaek@gmail.com>
2025-06-24 11:44:40 +0900

    Board entity 에 다대일 관계 추가

--- src/main/java/com/example/prj1/board/entity/Board.java
+++ src/main/java/com/example/prj1/board/entity/Board.java
@@ -1,5 +1,6 @@
 package com.example.prj1.board.entity;
 
+import com.example.prj1.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +20,10 @@ public class Board {
 
     private String title;
     private String content;
-    private String writer;
+
+    @ManyToOne
+    @JoinColumn(name = "writer")
+    private Member writer;
 
     @Column(name = "created_at", insertable = false, updatable = false)
     private LocalDateTime createdAt;
